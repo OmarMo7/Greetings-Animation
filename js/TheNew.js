c28592674fcd40c5f0062088800f2dfa489da3f4
OmarMo7 <omar7.first@gmail.com>
2021-05-09 05:05:55 +0200

    Merging body properties

--- js/TheNew.js
+++ js/TheNew.js
@@ -373,25 +373,25 @@ $(function () {
   var i = 1;
   var j = 1;
   setInterval(() => {
-    document.body.setAttribute('style', `background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='${width[j++]}' height='49' viewBox='0 0 28 49'%3E%3Cg fill-rule='evenodd'%3E%3Cg id='hexagons' fill='${colors[i++]}' fill-opacity='0.2' fill-rule='nonzero'%3E%3Cpath d='M13.99 9.25l13 7.5v15l-13 7.5L1 31.75v-15l12.99-7.5zM3 17.9v12.7l10.99 6.34 11-6.35V17.9l-11-6.34L3 17.9zM0 15l12.98-7.5V0h-2v6.35L0 12.69v2.3zm0 18.5L12.98 41v8h-2v-6.85L0 35.81v-2.3zM15 0v7.5L27.99 15H28v-2.31h-.01L17 6.35V0h-2zm0 49v-8l12.99-7.5H28v2.31h-.01L17 42.15V49h-2z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`)
     var styles = {
-      "transition": "all 1s ease-out"
+      "transition": "all 1s ease-out",
+      "background-image": `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='${width[j++]}' height='49' viewBox='0 0 28 49'%3E%3Cg fill-rule='evenodd'%3E%3Cg id='hexagons' fill='${colors[i++]}' fill-opacity='0.2' fill-rule='nonzero'%3E%3Cpath d='M13.99 9.25l13 7.5v15l-13 7.5L1 31.75v-15l12.99-7.5zM3 17.9v12.7l10.99 6.34 11-6.35V17.9l-11-6.34L3 17.9zM0 15l12.98-7.5V0h-2v6.35L0 12.69v2.3zm0 18.5L12.98 41v8h-2v-6.85L0 35.81v-2.3zM15 0v7.5L27.99 15H28v-2.31h-.01L17 6.35V0h-2zm0 49v-8l12.99-7.5H28v2.31h-.01L17 42.15V49h-2z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`
     };
 
     Object.assign(document.body.style, styles)
     if (i == colors.length) i = 0
     if (j == width.length) j = 0
   }, 4000)
-  // setInterval(() => {
-  //   // var backImg = document.querySelector('.te-cover img')
-  //   var imgs = document.querySelectorAll('img')
-  //   var num = randomNumber(0, 24);
-  //   imgs.forEach(img => {
-  //     if (!img.classList.contains('border-radius')) {
-  //       console.log(img)
-  //       img.classList.add('border-radius')
-  //     }
-  //   })
-  //   TransitionEffects.init(16); console.log("gonna");
-  // }, 4000)
+  setInterval(() => {
+    // var backImg = document.querySelector('.te-cover img')
+    var imgs = document.querySelectorAll('img')
+    var num = randomNumber(0, 24);
+    imgs.forEach(img => {
+      if (!img.classList.contains('border-radius')) {
+        console.log(img)
+        img.classList.add('border-radius')
+      }
+    })
+    TransitionEffects.init(16); console.log("gonna");
+  }, 4000)
 });
