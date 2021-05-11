ec8f69a86d85be27519b55fddd4ac79f3e71f447
OmarMo7 <omar7.first@gmail.com>
2021-05-11 16:54:58 +0200

    Performing snowflakes before & after HBD greetings

--- js/TheNew.js
+++ js/TheNew.js
@@ -388,8 +388,11 @@ $(function () {
     te_cover.classList.remove('te-hide')
     te_shadow.classList.remove('te-hide')
 
-    setTimeout(() => { TransitionEffects.init(17); }, 2000)
-    document.body.setAttribute('style', `background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='${width[0]}' height='49' viewBox='0 0 28 49'%3E%3Cg fill-rule='evenodd'%3E%3Cg id='hexagons' fill='${colors[0]}' fill-opacity='0.2' fill-rule='nonzero'%3E%3Cpath d='M13.99 9.25l13 7.5v15l-13 7.5L1 31.75v-15l12.99-7.5zM3 17.9v12.7l10.99 6.34 11-6.35V17.9l-11-6.34L3 17.9zM0 15l12.98-7.5V0h-2v6.35L0 12.69v2.3zm0 18.5L12.98 41v8h-2v-6.85L0 35.81v-2.3zM15 0v7.5L27.99 15H28v-2.31h-.01L17 6.35V0h-2zm0 49v-8l12.99-7.5H28v2.31h-.01L17 42.15V49h-2z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`)
+    setTimeout(() => {
+      TransitionEffects.init(17);
+      document.body.setAttribute('style', `background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='${width[0]}' height='49' viewBox='0 0 28 49'%3E%3Cg fill-rule='evenodd'%3E%3Cg id='hexagons' fill='${colors[0]}' fill-opacity='0.2' fill-rule='nonzero'%3E%3Cpath d='M13.99 9.25l13 7.5v15l-13 7.5L1 31.75v-15l12.99-7.5zM3 17.9v12.7l10.99 6.34 11-6.35V17.9l-11-6.34L3 17.9zM0 15l12.98-7.5V0h-2v6.35L0 12.69v2.3zm0 18.5L12.98 41v8h-2v-6.85L0 35.81v-2.3zM15 0v7.5L27.99 15H28v-2.31h-.01L17 6.35V0h-2zm0 49v-8l12.99-7.5H28v2.31h-.01L17 42.15V49h-2z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`)
+    }, 2000)
+
     var i = 1;
     var j = 1;
     setInterval(() => {
@@ -404,7 +407,6 @@ $(function () {
       if (j == width.length) j = 0
     }, 4000)
     setInterval(() => {
-      // var backImg = document.querySelector('.te-cover img')
       var imgs = document.querySelectorAll('img')
       var num = randomNumber(0, 24);
       imgs.forEach(img => {
@@ -437,7 +439,8 @@ $(function () {
   performLogin = function () {
     if (localStorage.getItem('name') != null && localStorage.getItem('password') != null &&
       localStorage.getItem('name') == Ezz.name && localStorage.getItem('password') == Ezz.password) {
-      performDancingText();
+      performDancingText()
+      performSnowFlake(1.2)
       setTimeout(() => {
         var container = document.querySelector('.container')
         var controls = document.querySelector('.te-controls')
@@ -454,7 +457,8 @@ $(function () {
         Object.assign(wrapper.style, displayNone)
         Object.assign(container.style, display)
         performAction();
-      }, 10 * 1000)
+        performSnowFlake(0.8);
+      }, 12 * 1000)
       localStorage.removeItem('name'); localStorage.removeItem('password');
       return;
     }
@@ -576,6 +580,39 @@ $(function () {
       "display": "none"
     };
     Object.assign(container.style, display)
+  }
+  performSnowFlake = function (font_size) {
+    var exist = false;
+    if (document.querySelector('.snowflake') != null) { exist = true }
+    if (!exist) {
+      var snowFlakes = document.createElement('div')
+      snowFlakes.classList.add('snowflakes')
+      snowFlakes.setAttribute('aria-hidden', 'true')
+      for (let i = 0; i < 10; i++) {
+        var snowFlake = document.createElement('div')
+        snowFlake.classList.add('snowflake')
+        var font = { "font-size": `${font_size}em` };
+        console.log(font_size)
+        Object.assign(snowFlake.style, font)
+        if (i == 0 || i % 3 == 0) { snowFlake.innerHTML = "&#128149" }
+        else if (i == 2 || i % 2 == 0) { snowFlake.innerHTML = "&#127802" }
+        else if (i == 1 || i % 1 == 0) {
+          snowFlake.innerHTML = "&#127880"
+        }
+        snowFlakes.appendChild(snowFlake);
+      }
+      document.querySelector('body').insertBefore(snowFlakes, document.querySelector('.wrapper'))
+    }
+    else {
+      snowFlakes = document.querySelector('.snowflakes')
+      snowFlake = document.querySelectorAll('.snowflake')
+      snowFlake.forEach(flake => {
+        var font = { "font-size": `${font_size}em` };
+        console.log(font_size)
+        Object.assign(flake.style, font)
+      })
+    }
+
   }
   verifyUser = function (user, pass) {
     if (user == "Zezo" && pass == "zzz") {
